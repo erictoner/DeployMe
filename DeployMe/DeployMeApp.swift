58dcc8d8ddd798a14b1b47cf1a7b4e7e91e8fd31
Eric Toner <erict@aweber.com>
2022-07-28 21:18:03 -0400

    Initial Commit

---
+++ DeployMe/DeployMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeployMeApp.swift
+//  DeployMe
+//
+//  Created by Eric Toner on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct DeployMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
